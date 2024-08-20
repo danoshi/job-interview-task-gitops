b4869fce178c501de159ef4f08143368a291a4ba
Daniel Kaminski <danielkaminski24@gmail.com>
2024-08-20 15:42:18 +0200

    add current changes

---
+++ terraform/kind/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "~> 0.0.19"
+    }
+  }
+}
+
+provider "kind" {}
+
+resource "kind_cluster" "default" {
+    name = var.name
+}
---
+++ terraform/kind/test.tfvars
@@ -0,0 +1,1 @@
+name = "test-whizu-corp"
