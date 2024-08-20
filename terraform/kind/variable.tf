b5cd1f2681e299aa45ab1f39fa9d5369ffa03d75
Daniel Kaminski <danielkaminski24@gmail.com>
2024-08-20 20:12:58 +0200

    modification to terraform script

--- terraform/kind/variable.tf
+++ terraform/kind/variable.tf
@@ -2,3 +2,9 @@ variable "name" {
   type = string
   description = "Name of kind cluster"
 }
+
+variable "kubeconfig_path" {
+  type = string
+  description = "Path to kubeconfig"
+  default = ".kube/config"
+}
